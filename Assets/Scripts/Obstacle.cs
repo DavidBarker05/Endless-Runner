9217730a49bac06b6e486d3c5bb47d827dc41042
DavidBarker05 <davidbarker9611@gmail.com>
2025-04-01 18:14:28 +0200

    Minor changes

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField]
+    GameManager gameManager;
+
     // OnTriggerEnter works with CharacterController, but OnCollisionEnter does not, so we use OnTriggerEnter
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             // Do obstacle killing stuff
+            // Work with game manager game states
         }
     }
 }
