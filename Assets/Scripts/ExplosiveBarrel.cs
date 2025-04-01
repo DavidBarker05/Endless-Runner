c13a7e2ca9a4fa65ff66025eed455d7da28042cc
DavidBarker05 <davidbarker9611@gmail.com>
2025-04-01 17:52:56 +0200

    Added explosion particles and made the barrel not attempt to explode itself

--- Assets/Scripts/ExplosiveBarrel.cs
+++ Assets/Scripts/ExplosiveBarrel.cs
@@ -1,13 +1,18 @@
 using Array = System.Array;
 using UnityEngine;
+using GameUtilities;
 
 public class ExplosiveBarrel : MonoBehaviour
 {
     [SerializeField]
     [Min(0.001f)]
     float explosionSize;
+    //[SerializeField]
+    ParticleSystem explosion;
     [SerializeField]
     LayerMask explodables;
+
+    void Awake() => explosion = UtilityMethods.GetParent(gameObject).GetComponentInChildren<ParticleSystem>();
 
     void OnTriggerEnter(Collider other)
     {
@@ -16,10 +21,14 @@ public class ExplosiveBarrel : MonoBehaviour
 
     public void Explode()
     {
+        gameObject.layer = 2;
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionSize, explodables, QueryTriggerInteraction.Collide);
         Collider player = Array.Find<Collider>(colliders, (Collider c) => c.CompareTag("Player"));
         // To-Do: Kill Player
         Collider[] barrels = Array.FindAll<Collider>(colliders, (Collider c) => c.CompareTag("ExplosiveBarrel"));
         Array.ForEach<Collider>(barrels, b => b.gameObject.GetComponent<ExplosiveBarrel>().Explode());
+        gameObject.SetActive(false);
+        explosion.Play();
+        Destroy(UtilityMethods.GetParent(gameObject), explosion.main.duration);
     }
 }
