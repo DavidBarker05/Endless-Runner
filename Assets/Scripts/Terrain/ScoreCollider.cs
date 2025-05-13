ba75291899f149344d525f0c89876242e9f9c887
DavidBarker05 <davidbarker9611@gmail.com>
2025-05-13 13:53:53 +0200

    Fixed score detection for security door

--- Assets/Scripts/Terrain/ScoreCollider.cs
+++ Assets/Scripts/Terrain/ScoreCollider.cs
@@ -9,6 +9,8 @@ public class ScoreCollider : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && GetComponentInParent<ObstacleRow>().HasObstacles) LevelManager.Score++;
+        if (!other.gameObject.CompareTag("Player")) return; // If doesn't collide with player then skip
+        if (GetComponentInParent<ObstacleRow>() != null && !GetComponentInParent<ObstacleRow>().HasObstacles) return; // If has obstacle rows but no obstacles then skip
+        LevelManager.Score++; // If there are obstacles or it's a security door then increase score
     }
 }
