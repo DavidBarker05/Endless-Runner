7a557d7cad39ee1245b89ad48f74fc9e366973f6
DavidBarker05 <davidbarker9611@gmail.com>
2025-06-02 18:53:06 +0200

    removed comment

--- Assets/Scripts/Level/Terrain/ScoreCollider.cs
+++ Assets/Scripts/Level/Terrain/ScoreCollider.cs
@@ -10,6 +10,5 @@ public class ScoreCollider : MonoBehaviour
         if (!other.gameObject.CompareTag("Player")) return; // If doesn't collide with player then skip
         if (GetComponentInParent<ObstacleRow>() != null && !GetComponentInParent<ObstacleRow>().HasObstacles) return; // If has obstacle rows but no obstacles then skip
         GameManager.Instance.InvokeEvent(GameUtilities.GameEvents.EventType.ObstaclePassed, this, LevelManager.Instance.Score + 1); // If there are obstacles or it's a security door then increase score
-        //LevelManager.Instance.Score++; // If there are obstacles or it's a security door then increase score
     }
 }
