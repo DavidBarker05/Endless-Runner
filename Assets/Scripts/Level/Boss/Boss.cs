27a7d0847c2f18740d3d8569ab6224e4ae1b4953
DavidBarker05 <davidbarker9611@gmail.com>
2025-06-03 18:24:28 +0200

    got rid of para for single line comments

--- Assets/Scripts/Level/Boss/Boss.cs
+++ Assets/Scripts/Level/Boss/Boss.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
 
 /// <summary>
-/// <para>
 /// The base abstract class that child <see cref="Boss"/> scripts will derive from.
-/// </para>
 /// </summary>
 public abstract class Boss : MonoBehaviour
 {
     /// <summary>
-    /// <para>
     /// Begin the coroutine to deactivate the boss.
-    /// </para>
     /// </summary>
     public virtual void Disengage() => StartCoroutine(Deactivate());
 
