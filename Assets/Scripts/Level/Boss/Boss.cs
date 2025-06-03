fd464c7c7631140f429939fc94066be89039dcd3
DavidBarker05 <davidbarker9611@gmail.com>
2025-06-03 17:27:38 +0200

    removed private keyword
    not really needed

--- Assets/Scripts/Level/Boss/Boss.cs
+++ Assets/Scripts/Level/Boss/Boss.cs
@@ -4,7 +4,7 @@ public abstract class Boss : MonoBehaviour
 {
     public virtual void Disengage() => StartCoroutine(Deactivate());
 
-    private System.Collections.IEnumerator Deactivate()
+    System.Collections.IEnumerator Deactivate()
     {
         yield return new WaitForSeconds(2.5f);
         Destroy(gameObject);
