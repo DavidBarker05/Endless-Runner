f74a18743f0cbcc8e0b48d306cfa9b7217a05d8e
DavidBarker05 <davidbarker9611@gmail.com>
2025-06-03 17:29:38 +0200

    Added comments

--- Assets/Scripts/Level/Boss/Boss.cs
+++ Assets/Scripts/Level/Boss/Boss.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
+/// <summary>
+/// The base abstract class that the first and second boss scripts derive from
+/// </summary>
 public abstract class Boss : MonoBehaviour
 {
+    /// <summary>
+    /// Begin the coroutine to deactivate the boss
+    /// </summary>
     public virtual void Disengage() => StartCoroutine(Deactivate());
 
+    // Deactivate (destroy) the boss after a certain number of seconds
     System.Collections.IEnumerator Deactivate()
     {
         yield return new WaitForSeconds(2.5f);
