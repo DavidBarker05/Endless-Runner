d0275965d54fdbefcbf54e5a301b3b729b7087cc
DavidBarker05 <davidbarker9611@gmail.com>
2025-06-14 17:05:04 +0200

    added code to start wallrun

--- Assets/Scripts/Level/Terrain/WallrunSurface.cs
+++ Assets/Scripts/Level/Terrain/WallrunSurface.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class WallrunSurface : MonoBehaviour
@@ -10,36 +11,39 @@ public class WallrunSurface : MonoBehaviour
 
     [SerializeField]
     SurfaceSide surfaceSide;
+    [SerializeField]
+    float wallrunY;
+    [SerializeField]
+    float yTransitionTime;
 
-    bool isWallrunning = false;
-
-    void Awake()
-    {
-    }
-
-    void Start()
-    {
-    }
-
-    void Update()
-    {
-    }
-
-    void FixedUpdate()
-    {
-    }
+    Coroutine changePlayerY;
 
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         PlayerManager.Instance.State = surfaceSide == SurfaceSide.Right ? PlayerManager.AnimationState.WallrunRight : PlayerManager.AnimationState.WallrunLeft;
-        isWallrunning = true;
+        if (changePlayerY != null) StopCoroutine(changePlayerY);
+        changePlayerY = StartCoroutine(ChangePlayerY());
     }
 
     void OnTriggerExit(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         PlayerManager.Instance.State = PlayerManager.AnimationState.Fall;
-        isWallrunning = false;
+        if(changePlayerY != null) StopCoroutine(changePlayerY);
+    }
+
+    IEnumerator ChangePlayerY()
+    {
+        Vector3 target = new Vector3(PlayerManager.Instance.transform.position.x, Mathf.Max(PlayerManager.Instance.transform.position.y, wallrunY));
+        while (PlayerManager.Instance.transform.position.y < target.y)
+        {
+            float y = target.y - PlayerManager.Instance.transform.position.y;
+            float dy = (y / yTransitionTime) * Time.fixedDeltaTime;
+            Vector3 dyVector = GameUtilities.UtilityMethods.UtilMethods.YVector(dy);
+            PlayerManager.Instance.transform.position += dyVector;
+            yield return new WaitForFixedUpdate();
+        }
+        PlayerManager.Instance.transform.position = target;
     }
 }
